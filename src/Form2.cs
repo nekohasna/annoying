c419dc0d5ac0f21dad619efbcfaa6ffbc112fd8f
nekohasna <79271838+nekohasna@users.noreply.github.com>
2022-11-03 13:10:21 +0800

    progress 1

---
+++ src/Form2.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace annoying.src
+{
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        private void Form2_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Search_data_Click(object sender, EventArgs e)
+        {
+            string city = txt_city.Text;
+            try
+            {
+                StringBuilder queryaddress = new StringBuilder();
+                queryaddress.Append("http://maps.google.com/maps?q=");
+
+                if(city != string.Empty)
+                {
+                    queryaddress.Append(city + "," + "+");
+                }
+
+                webBrowser1.Navigate(queryaddress.ToString());
+            }
+            catch (Exception ex)
+            {
+
+                MessageBox.Show(ex.Message.ToString(), "Error");
+            }
+        }
+
+        private void splitContainer1_Panel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void splitContainer1_SplitterMoved(object sender, SplitterEventArgs e)
+        {
+
+        }
+    }
+}
